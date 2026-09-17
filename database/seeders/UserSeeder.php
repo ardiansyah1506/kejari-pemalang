d9f93a56e24e0c622bf98709913f9a0c26e278e1
ardiansyah1506 <fauziardiansyah302@gmail.com>
2026-09-18 06:12:22 +0700

    Registers User seeder for initialization
    Integrates the User seeder into the primary database seeding process. This ensures that essential user data is created automatically during database setup, providing a complete dataset for development and testing environments.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'username' => 'admin',
+            'password' => Hash::make('password')
+        ]);
+        
+        User::create([
+            'username' => 'ziii',
+            'password' => Hash::make('ziii123')
+        ]);
+    }
+}
